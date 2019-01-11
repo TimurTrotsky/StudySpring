38d2ae192760f2a8742d07a23700ce7495ad34c4
TimurTrotsky <44401695+TimurTrotsky@users.noreply.github.com>
2019-01-11 23:13:34 +0300

    v1

---
+++ src/main/java/defoultpack/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package defoultpack.repos;
+
+import defoultpack.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List <Message> findByTag(String tag);
+}
